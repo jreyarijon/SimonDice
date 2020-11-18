29eabc4b2a16bbc41c042d5b651a77bb72fc032e
Josemolamazo <Jreyarijon@danielcastelao.org>
2020-11-18 12:28:30 +0100

    Initial commit + Curoutines

---
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.simondice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+       
+    }
+}
+
